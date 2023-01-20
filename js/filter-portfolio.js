f0f7abee487ca73a4e6c9ac4671d0f7983e4d7a1
Serhii <97292099+Brute1996@users.noreply.github.com>
2023-01-20 17:34:04 +0200

    add filter

--- js/filter-portfolio.js
+++ js/filter-portfolio.js
@@ -1,10 +1,14 @@
 const filterButtons = document.querySelector('.filter-btn-list')
-const porfolioList = document.querySelector('.portf-list')
+const portfolioCards = document.querySelectorAll('.portf-item')
 filterButtons.addEventListener('click', (e) => {
     if (e.target.nodeName !== 'BUTTON') {
         return;
     };
-    console.dir(porfolioList.children.forEach(child => {
-        console.log(child.dataset === e.target.dataset);
-    }))
-})
+
+    portfolioCards.forEach(card =>{
+        if (card.dataset.content !== e.target.dataset.filter && e.target.dataset.filter !== 'all'){
+            card.classList.add('disable-element');
+        } else card.classList.remove('disable-element');
+        
+    });
+});
